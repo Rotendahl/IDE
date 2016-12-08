6b01161061ac13f7b230d7ceff44815d3765022c
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2016-12-08 14:21:15 +0100

    Working commit

--- data/parser.py
+++ data/parser.py
@@ -11,34 +11,40 @@ with  open('movie1.csv', 'rb') as f:
 
 
 
-
+actorsIndex = []
 actors = []
 links  = []
 movies.pop(0)
 
 for entry in movies:
     if not(entry[0] in actors):
-        actors.append(entry[0])
+        actors.append({'name' : entry[0]})
+        actorsIndex.append(entry[0])
     if not(entry[1] in actors):
-        actors.append(entry[1])
+        actors.append({'name' : entry[1]})
+        actorsIndex.append(entry[1])
     if not(entry[3] in actors):
-        actors.append(entry[3])
+        actors.append({'name' : entry[3]})
+        actorsIndex.append(entry[3])
 
 for entry in movies:
     links.append({
         'title'  : entry[2],
         'source' : actors.index(entry[0]),
-        'target' : actors.index(entry[1])
+        'target' : actorsIndex.index(entry[1]),
+        'value'  : 1
     })
     links.append({
         'title'  : entry[2],
-        'source' : actors.index(entry[0]),
-        'target' : actors.index(entry[3])
+        'source' : actorsIndex.index(entry[0]),
+        'target' : actorsIndex.index(entry[3]),
+        'value'  : 1
     })
     links.append({
         'title'  : entry[2],
-        'source' : actors.index(entry[1]),
-        'target' : actors.index(entry[3])
+        'source' : actorsIndex.index(entry[1]),
+        'target' : actorsIndex.index(entry[3]),
+        'value'  : 1
     })
 
 

9bd6b68e007ff98df29ea32d0e3ce5ae07b4f0e6
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2016-12-19 16:47:24 +0100

    Koden er done

--- assignments/4/code.js
+++ assignments/4/code.js
@@ -1,7 +1,6 @@
 d3.select(window).on('load', init);
 
 var w;
-console.log(w)
 var h = 700;
 var padding = 20;
 
@@ -16,6 +15,7 @@ var handCircles;
 var handLines;
 var handFigure;
 var pointGroup;
+var points = [];
 
 
 var primaryColor   = "#4c6aff";
@@ -37,7 +37,7 @@ function parseHand(hand){
     }
 
 function init() {
-    w = document.getElementById('handViz').offsetWidth;
+    w = document.getElementById('handViz').offsetWidth - padding;
     var svg = d3.select("#handViz").append('svg').attr("width",w).attr('height',h);
     var my_data = d3.text("../../data/hands.csv", function(text){
         var data = d3.csv.parseRows(text).map(function(row) {
@@ -123,114 +123,109 @@ function init() {
         });
 
 
-        data = d3.transpose(data);
 
-        data = data.slice(0,40);
+        for (var i = 0; i < data.length; i++) {
+            points[i] =  {'x' : data[i][0], 'y' : data[i][1]}
+        }
 
-        var minX= d3.min(data[0], function(d) {
-                  return +d;} );
-        var maxX= d3.max(data[0], function(d) {
-                  return +d;} );
-        var minY= d3.min(data[1], function(d) {
-                  return +d;} );
-        var maxY= d3.max(data[1], function(d) {
-                  return +d;} );
-        console.log(minX);
-        console.log(maxX);
-        console.log(minY);
-        console.log(maxY);
+        var minX= d3.min(points, function(d) {return +d.x;});
+        var maxX= d3.max(points, function(d) {return +d.x;});
+        var minY= d3.min(points, function(d) {return +d.y;});
+        var maxY= d3.max(points, function(d) {return +d.y});
 
 
         var sx = d3.scale.linear()
             .domain([minX,maxX])
-            .range([w/2, w]);
+            .range([w/2 + padding, w - padding]);
 
         var sy = d3.scale.linear()
             .domain([minY,maxY])
-            .range([h,0]);
+            .range([h-  padding,0 + padding])
+            ;
 
+        console.log(sx(-0.48998377) + " og " + w/2)
 
         pointGroup = svg.append('g').attr("id", "pointGroup");
-
         pointGroup.selectAll('circle')
-            .data(data)
+            .data(points)
           .enter().append('circle')
             .attr('r', '5')
-            .attr('cx', function(d){ return sx(d[0]); })
-            .attr('cy', function(d){ return sy(d[1]); })
-            .style("fill", "red")
-            .on("mouseover", function(d) {
+            .attr('cx', function(d){ return sx(d.x); })
+            .attr('cy', function(d){ return sy(d.y); })
+            .style("fill", primaryColor)
+            .on("mouseover", function(d, i ) {
+                console.log(i);
+
               div.transition()
                   .duration(200)
                   .style("opacity", .9);
-              div .html("(" + d3.round(d[0],2) + ", " + d3.round(d[1],2) + ")")
+              div .html("(" + d3.round(d.x,2) + ", " + d3.round(d.y,2) + ")"+
+                        "<br>Hand number: " + i)
                   .style("left", (d3.event.pageX) + "px")
                   .style("top", (d3.event.pageY - 28) + "px");
+
             })
             .on("mouseout", function(d) {
               div.transition()
                   .duration(500)
                   .style("opacity", 0);
-            });
-
-    });
-
-
-    d3.select('h6').on('click', function(){
-        currentHandIndex = Math.floor(Math.random()*hands.length)
-        console.log("Current Index: "  + currentHandIndex);
-        hand = hands[currentHandIndex];
-
-        handLines.selectAll('line').data(hand)
-        .transition()
-        .duration(500)
-        .ease("elastic")
-        .each("start", function(){
-            d3.select(this)
-            .attr('stroke-width', lineWidth * 2)
-            .attr("stroke", complemColor);})
-            .attr('x1', function(d, i){
-                if(i > 0){
-                    return xScale(hands[currentHandIndex][i-1].x);
-                }
-                else{
-                    return xScale(d.x);
-                }
-                })
-            .attr('y1', function(d, i){
-                if(i > 0){
-                    return yScale(hands[currentHandIndex][i-1].y);
-                }
-                else{
-                    return yScale(d.y);
-                }
             })
-            .attr('x2', function(d){ return xScale(d.x)})
-            .attr('y2', function(d){ return yScale(d.y)
-        })
-        .each('end', function(){
-            d3.select(this)
-            .transition()
-            .duration(100)
-            .attr('stroke-width', lineWidth)
-            .attr('stroke', secondaryColor)}
-        );
+            .on('click', function(){
+                currentHandIndex = Math.floor(Math.random()*hands.length)
+                console.log("Current Index: "  + currentHandIndex);
+                hand = hands[currentHandIndex];
 
-        handCircles.selectAll('circle').data(hand)
-        .transition()
-        .duration(500)
-        .ease("elastic")
-        .each("start", function(){d3.select(this)
-                .attr('r', cirleRad*2)
-                .attr("fill", complemColor);})
-        .attr('cx', function(d){return xScale(d.x); })
-        .attr('cy', function(d){return yScale(d.y); })
-        .each('end', function(){
-            d3.select(this)
-            .transition()
-            .duration(100)
-            .attr('fill', secondaryColor)
-            .attr('r', '3')
+                handLines.selectAll('line').data(hand)
+                .transition()
+                .duration(500)
+                .ease("elastic")
+                .each("start", function(){
+                    d3.select(this)
+                    .attr('stroke-width', lineWidth * 2)
+                    .attr("stroke", complemColor);})
+                    .attr('x1', function(d, i){
+                        if(i > 0){
+                            return xScale(hands[currentHandIndex][i-1].x);
+                        }
+                        else{
+                            return xScale(d.x);
+                        }
+                        })
+                    .attr('y1', function(d, i){
+                        if(i > 0){
+                            return yScale(hands[currentHandIndex][i-1].y);
+                        }
+                        else{
+                            return yScale(d.y);
+                        }
+                    })
+                    .attr('x2', function(d){ return xScale(d.x)})
+                    .attr('y2', function(d){ return yScale(d.y)
+                })
+                .each('end', function(){
+                    d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr('stroke-width', lineWidth)
+                    .attr('stroke', secondaryColor)}
+                );
+
+                handCircles.selectAll('circle').data(hand)
+                .transition()
+                .duration(500)
+                .ease("elastic")
+                .each("start", function(){d3.select(this)
+                        .attr('r', cirleRad*2)
+                        .attr("fill", complemColor);})
+                .attr('cx', function(d){return xScale(d.x); })
+                .attr('cy', function(d){return yScale(d.y); })
+                .each('end', function(){
+                    d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr('fill', secondaryColor)
+                    .attr('r', '3')
+                    });
             });
     });
 };
